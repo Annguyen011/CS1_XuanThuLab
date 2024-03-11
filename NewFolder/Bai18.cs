9faeb805006eed1661e7e27ad02d68f81044a1dc
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-11 23:12:18 +0700

    Add

--- NewFolder/Bai18.cs
+++ NewFolder/Bai18.cs
@@ -8,6 +8,16 @@ namespace XuanLab.NewFolder
 {
     internal class Bai18
     {
+        class A
+        {
+            public string Name { get; set; }
+        }
+        void Main()
+        {
+            A a = null;
+            int? age;
+            age = null;
 
+        }
     }
 }
