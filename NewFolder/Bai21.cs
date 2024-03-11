f73e2c8fbf7b5e2ce421d69efc9d57ae5089b4f0
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-11 23:47:26 +0700

    Add

--- NewFolder/Bai21.cs
+++ NewFolder/Bai21.cs
@@ -8,5 +8,31 @@ namespace XuanLab.NewFolder
 {
     internal class Bai21
     {
+        /*
+         * 1) 
+         * (Tham so) => bieuThuc
+         * 
+         * 2)
+         * (Tham so) => 
+         * {
+         *  cac bieu thuc
+         *  return kieu tra ve
+           }
+        */
+        void Main()
+        {
+            Action<string> s1 = s => Console.WriteLine(s); 
+            Action<string> s2 = (string s) => Console.WriteLine(s); 
+        
+            Action action = () => Console.WriteLine("Hello");
+
+            int[] arr = {1, 2, 3 };
+        
+
+            var change = arr.Select( x => 
+            {
+                return Math.Sqrt(x);
+            });
+        }   
     }
 }
