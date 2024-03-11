43988c0034f5635daaddf5c31c0b2f116200c071
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-11 23:18:22 +0700

    Add

---
+++ NewFolder/Bai19.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuanLab.NewFolder
+{
+    internal class Bai19
+    {
+        // Neu su dung abstact thi lop con bat buoc phai ke thua lai
+        // phuong thuc do cua lop cha
+        class Product
+        {
+            protected double Price;
+            
+            public void ProductInfo()
+            {
+                
+            }
+        }
+
+        class Iphone : Product
+        {
+
+        }
+    }
+}
