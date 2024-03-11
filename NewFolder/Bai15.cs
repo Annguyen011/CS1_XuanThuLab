9c6c5c9c0378960f1dfd69cfeef190014825e435
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-11 17:47:45 +0700

    Add

--- NewFolder/Bai15.cs
+++ NewFolder/Bai15.cs
@@ -8,5 +8,6 @@ namespace XuanLab.NewFolder
 {
     internal class Bai15
     {
+
     }
 }
---
+++ NewFolder/Bai16.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuanLab.NewFolder
+{
+    internal class Bai16
+    {
+        // Nap chong phuong thuc voi cac ham cung ten nhung khac tham so 
+        // hoac la khac phuong thuc
+        // 
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        static void Swap(ref float a, ref float b)
+        {
+            float temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        
+    }
+}
