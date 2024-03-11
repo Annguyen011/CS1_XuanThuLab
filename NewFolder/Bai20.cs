81cd467a596badbcff8abe9bb83129be68de0d63
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-11 23:34:40 +0700

    Add

--- NewFolder/Bai20.cs
+++ NewFolder/Bai20.cs
@@ -8,5 +8,30 @@ namespace XuanLab.NewFolder
 {
     internal class Bai20
     {
+        // Delegate
+
+        public delegate void ShoLog(string message);
+
+        static void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        void Main(string[] args)
+        {
+            ShoLog log = null;
+            //Gan cho delegate
+            // Co the gan cho nhieu phuong thuc cho delegate
+            log += Info;
+
+            // Hai cach viet tuong tu nhau
+            log.Invoke("Hi");
+
+            Action action; // ~ delegate void khong tham so
+            Action<string, int> action1; // ~ delegate void string int
+
+            Func<int> func; // ~ delegate int
+            Func<float, string, int> func2; // ~ delegate int (float, string)
+        }
     }
 }
--- obj/Debug/net8.0/XuanLab.AssemblyInfo.cs
+++ obj/Debug/net8.0/XuanLab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XuanLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9faeb805006eed1661e7e27ad02d68f81044a1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43988c0034f5635daaddf5c31c0b2f116200c071")]
 [assembly: System.Reflection.AssemblyProductAttribute("XuanLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XuanLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
